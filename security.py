ea0142a6027a55f28a61a2881deb5d3b5548f175
unknown <r.sultanzada@pomtech.com>
2021-09-28 16:51:03 +0430

    First Commit

---
+++ security.py
@@ -0,0 +1,13 @@
+from werkzeug.security import safe_str_cmp
+from mycode.models.userone import UserModel
+
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if user and user.password == password:
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
